da8181a8179eb64784b8274ad2518b82fae6c457
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-27 12:25:56 +0300

    AnimatedStateListDrawable

---
+++ app/src/main/java/com/example/animator/animated_state_list_drawable/AnimatedStateListDrawableActivity.kt
@@ -0,0 +1,13 @@
+package com.example.animator.animated_state_list_drawable
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.animator.R
+
+class AnimatedStateListDrawableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animated_state_list_drawable)
+    }
+}
