ee1f04a017fd9e3e0d053494f963dc04c521b18f
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-28 10:38:43 +0300

    AnimatedVectorDrawable

---
+++ app/src/main/java/com/example/animator/animated_vector/AnimatedVectorDrawableActivity.kt
@@ -0,0 +1,21 @@
+package com.example.animator.animated_vector
+
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.animator.R
+import kotlinx.android.synthetic.main.activity_animated_vector_drawable.*
+
+class AnimatedVectorDrawableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animated_vector_drawable)
+
+        val animatedVectorDrawable = ivDemo.drawable as AnimatedVectorDrawable
+
+        ivDemo.setOnClickListener {
+            animatedVectorDrawable.start()
+        }
+    }
+}
